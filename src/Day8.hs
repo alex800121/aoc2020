fd0a7fdfbdcfcdf4e9a84add655b035a7d419632
alex800121 <alex800121@hotmail.com>
2023-09-19 03:26:50 +0800

    day9

--- src/Day8.hs
+++ src/Day8.hs
@@ -1,8 +1,11 @@
 module Day8 where
 
+import MyLib (firstRepeatBy)
 import Data.Array.IArray
 import Control.Monad (foldM)
-import Data.List (unfoldr)
+import Data.List (unfoldr, find)
+import Data.Function (on)
+import Data.Maybe (isNothing)
 
 data Instruction a
   = Acc a
@@ -31,8 +34,26 @@ run (M acc ins n)
   where
     (lowerB, upperB) = bounds ins
 
+changeOne :: Array Int (Instruction Int) -> [Array Int (Instruction Int)]
+changeOne a = a'
+  where
+    b = bounds a
+    l = assocs a
+    f = do
+      x <- l
+      case x of
+        (i, Nop y) -> return (i, Jmp y)
+        (i, Jmp y) -> return (i, Nop y)
+        y -> return y
+    a' = [a // [x] | x <- f]
+
 day8 :: IO ()
 day8 = do
   input <- zip [0..] . map insParser . lines <$> readFile "input/input8.txt"
   let initMachine = M 0 (array (0, length input - 1) input) 0
-  print $ take 20 $ unfoldr (fmap (\x -> (x, x)) . run) initMachine
+      l = unfoldr (fmap (\x -> (x, x)) . run) initMachine
+      x = _acc . (l !!) . subtract 1 . fst <$> firstRepeatBy ((==) `on` _n) l
+      initMachines = map (\x -> initMachine {_ins = x}) $ changeOne $ _ins initMachine
+  putStrLn $ ("day8a: " ++) $ show x
+  putStrLn $ ("day8b: " ++) $ show $ fmap (_acc . last) $ find (isNothing . firstRepeatBy ((==) `on` _n)) $ map (unfoldr (fmap (\x -> (x, x)) . run)) initMachines
+  
