7c942c86430d58d42d2b1969382b9720e6e00a30
alex800121 <alex800121@hotmail.com>
2023-09-19 19:06:23 +0800

    day9 done

--- src/Day10.hs
+++ src/Day10.hs
@@ -1,4 +1,10 @@
 module Day10 where
+import Data.List (sort, group)
+import Data.List.Split (divvy)
+
 
 day10 :: IO ()
-day10 = return ()
+day10 = do
+  input <- sort . map (read @Int) . lines <$> readFile "input/input10.txt"
+  let day10a = map length $ group $ sort $ map (subtract <$> head <*> (!! 1)) $ divvy 2 1 input
+  print day10a
