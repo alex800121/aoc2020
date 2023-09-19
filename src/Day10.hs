b97cb1536c68a6d7f790bbf78afc430438907d05
alex800121 <alex800121@hotmail.com>
2023-09-20 07:38:57 +0800

    done day12

--- src/Day10.hs
+++ src/Day10.hs
@@ -1,10 +1,23 @@
 module Day10 where
-import Data.List (sort, group)
-import Data.List.Split (divvy)
 
+import Data.List (group, sort, unfoldr, find)
+import Data.List.Split (divvy)
 
 day10 :: IO ()
 day10 = do
-  input <- sort . map (read @Int) . lines <$> readFile "input/input10.txt"
-  let day10a = map length $ group $ sort $ map (subtract <$> head <*> (!! 1)) $ divvy 2 1 input
-  print day10a
+  -- input' <- sort . map (read @Int) . lines <$> readFile "input/test10.txt"
+  input' <- sort . map (read @Int) . lines <$> readFile "input/input10.txt"
+  let input = 0 : input' ++ [last input' + 3]
+      day10a = product $ map length $ group $ sort $ map (subtract <$> head <*> (!! 1)) $ divvy 2 1 input
+      target = last input
+      day10b =
+        find ((== target) . fst) $
+          unfoldr
+            ( \(a, b, c) ->
+                let i = fst c + 1
+                    d = if i `elem` input then sum $ map snd [a, b, c] else 0
+                 in Just (c, (b, c, (i, d)))
+            )
+            ((-2, 0), (-1, 0), (0, 1))
+  putStrLn $ ("day10a: " ++) $ show day10a
+  putStrLn $ ("day10b: " ++) $ show day10b
--- src/Day11.hs
+++ src/Day11.hs
@@ -1,4 +1,44 @@
+{-# LANGUAGE LambdaCase #-}
 module Day11 where
 
+import Data.Map (Map)
+import qualified Data.Map as Map
+import MyLib (drawMap, stablized, drawGraph)
+import Data.Maybe (mapMaybe, fromMaybe)
+import Data.Bifunctor (Bifunctor(bimap))
+import Control.Monad (join)
+
+type Index = (Int, Int)
+type SeatMap = Map Index (Maybe Bool)
+
+step :: SeatMap -> SeatMap
+step s = Map.mapWithKey (fmap . f) s
+  where
+    f k True = 4 > length (filter id (mapMaybe (join . (s Map.!?) . bimap (+ fst k) (+ snd k)) surrounding))
+    f k False = all not (mapMaybe (join . (s Map.!?) . bimap (+ fst k) (+ snd k)) surrounding)
+
+step' :: SeatMap -> SeatMap
+step' s = Map.mapWithKey f s
+  where
+    f _ Nothing = Nothing
+    f k (Just True) = Just $ 5 > length (filter (seeOccupied s k) surrounding)
+    f k (Just False) = Just $ not (any (seeOccupied s k) surrounding)
+
+seeOccupied :: SeatMap -> Index -> Index -> Bool
+seeOccupied s i (x, y) = go (bimap (+ x) (+ y) i)
+  where
+    go n = case s Map.!? n of
+      Nothing -> False
+      Just Nothing -> go (bimap (+ x) (+ y) n)
+      Just (Just True) -> True
+      Just (Just False) -> False
+
+surrounding :: [Index]
+surrounding = [(x, y) | x <- [-1..1], y <- [-1..1], (x, y) /= (0, 0)]
+
 day11 :: IO ()
-day11 = return ()
+day11 = do
+  input <- drawMap (\case ; 'L' -> Just (Just False) ; '#' -> Just (Just True) ; _ -> (Just Nothing)) . lines <$> readFile "input/input11.txt"
+  -- input <- drawMap (\case ; 'L' -> Just (Just False) ; '#' -> Just (Just True) ; _ -> (Just Nothing)) . lines <$> readFile "input/test11.txt"
+  putStrLn $ ("day11a: " ++) $ show $ fmap (Map.size . Map.filter (fromMaybe False) . snd) $ stablized $ iterate step input
+  putStrLn $ ("day11b: " ++) $ show $ fmap (Map.size . Map.filter (fromMaybe False) . snd) $ stablized $ iterate step' input
