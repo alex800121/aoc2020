800f173c5389603bf129dbfc75e2d77eaf414791
alex800121 <alex800121@hotmail.com>
2023-09-19 19:06:23 +0800

    day9 done

--- src/Day9.hs
+++ src/Day9.hs
@@ -1,21 +1,20 @@
 module Day9 where
 
+import Control.Monad (guard, join)
+import Data.List (find, permutations, sort, tails, uncons, (\\))
 import Data.List.Split (divvy)
-import MyLib (pick)
-import Data.List (find, permutations, (\\), sort, uncons, tails)
 import Data.Maybe (mapMaybe)
-import Control.Monad (guard)
+import Debug.Trace (traceShow)
+import MyLib (pick)
 
-targetSum :: (Num a, Ord a) => a -> [a] -> [[a]]
-targetSum target source = go target (reverse $ sort source)
+targetSum :: (Num a, Ord a) => a -> [a] -> Maybe [a]
+targetSum target source = go (target - head source) 0 0
   where
-    go t s
-      | null s || t < 0 = []
-      | t == 0 = [[]]
-      | otherwise = do
-          (x, xs) <- mapMaybe uncons $ tails s
-          guard $ x <= t
-          (x :) <$> go (t - x) xs
+    go x i j
+      | j >= length source || i > j = Nothing
+      | x == 0 = Just $ map (source !!) [i .. j]
+      | x < 0 = go (x + source !! i) (i + 1) j
+      | x > 0 = go (x - source !! (j + 1)) i (j + 1)
 
 day9 :: IO ()
 day9 = do
@@ -24,4 +23,4 @@ day9 = do
       x = drop 25 input
       day9a = fmap fst $ find (not . snd) $ zipWith (\a b -> (a, a `elem` map sum (filter ((/=) <$> head <*> (!! 1)) $ pick 2 b))) x xs
   putStrLn $ ("day9a: " ++) $ show day9a
-  putStrLn $ ("day9b: " ++) $ show $ fmap (((+) <$> maximum <*> minimum) . head) $ targetSum <$> day9a <*> pure (filter ((< day9a) . Just) input)
+  putStrLn $ ("day9b: " ++) $ show $ fmap ((+) <$> minimum <*> maximum) $ join $ targetSum <$> day9a <*> pure input
