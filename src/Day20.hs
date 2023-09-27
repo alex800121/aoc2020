f7589b2d315577bbfe4301cb8f03ef591b92b846
alex800121 <alex800121@hotmail.com>
2023-09-28 03:00:27 +0800

    day20 pending stitch

--- src/Day20.hs
+++ src/Day20.hs
@@ -3,20 +3,27 @@
 
 module Day20 where
 
-import Control.Applicative (liftA, liftA2)
 import Control.Monad (guard)
 import Data.Array.IArray
 import Data.Array.Unboxed
 import Data.Bifunctor (bimap)
 import Data.Char (isNumber)
+import Data.Either (partitionEithers)
 import Data.Function ((&))
 import Data.List (delete, foldl', nub, (\\))
 import Data.List.Split (splitOn)
+import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Maybe (catMaybes, isJust, listToMaybe, mapMaybe)
+import Data.Set (Set)
+import qualified Data.Set as Set
 import Data.Tuple (swap)
 import Debug.Trace (traceShow)
 import MyLib (Direction (..), drawGraph, drawMap)
+
+type Pic = UArray Index Tile
+
+type PicMap = Map Index Tile
 
 type Tile = UArray Index Bool
 
@@ -29,6 +36,24 @@ type Index = (Int, Int)
 data Op = Flip | Rotate deriving (Show, Eq, Ord)
 
 data Op' a = Op {flipN :: a, rotateN :: a} deriving (Show, Eq, Ord, Functor)
+
+picMap :: [Tile] -> PicMap
+picMap [] = Map.empty
+picMap (x : xs) = go Map.empty (Set.singleton ((0, 0), x)) xs
+  where
+    go :: PicMap -> Set (Index, Tile) -> [Tile] -> PicMap
+    -- go m t ts | traceShow (Map.keys m) False = undefined
+    go m t ts = case ts of
+      [] -> m'
+      tss ->
+        let (ts', t') = fmap Set.fromList $ partitionEithers $ map (f (Set.toList t)) tss
+            f [] a = Left a
+            f (((xb, yb), b) : bs) a = case sharedSides b a of
+              Nothing -> f bs a
+              Just (a', (xa, ya)) -> Right ((xa + xb, ya + yb), a')
+         in go m' t' ts'
+      where
+        m' = Set.foldr (uncurry Map.insert) m t
 
 isTypeIn :: Tile -> [Tile] -> TileType
 isTypeIn t ts = case s of
@@ -46,7 +71,8 @@ tileVar t = matchT
     opList = [Op x y | x <- [0, 1], y <- [0 .. 3]]
     matchT = map (\x -> (x, op x t)) opList
 
-sharedSides :: Tile -> Tile -> Maybe (Tile, Tile, Index)
+sharedSides :: Tile -> Tile -> Maybe (Tile, Index)
+sharedSides ref t | ref == t = Nothing
 sharedSides ref t = matchSide
   where
     rVar = tileVar ref
@@ -60,38 +86,38 @@ sharedSides ref t = matchSide
             East -> (1, 0)
             South -> (0, 1)
             West -> (-1, 0)
-      return (ref, reverseOp rOp t, reverseIndex rOp rIndex)
+      return (reverseOp rOp t, reverseIndex rOp rIndex)
 
 reverseIndex :: Op' Int -> Index -> Index
 reverseIndex (Op a b) i =
-  foldl'
-    (\acc x -> x acc)
+  foldr
+    ($)
     i
-    (replicate (a `mod` 2) flipIndex <> replicate (negate b `mod` 4) rotateIndex )
+    (replicate (a `mod` 2) flipIndex <> replicate (negate b `mod` 4) rotateIndex)
   where
     flipIndex (x, y) = (-x, y)
     rotateIndex (x, y) = (-y, x)
 
 op, reverseOp :: Op' Int -> Tile -> Tile
 op (Op a b) t =
-  foldl'
-    (\acc x -> x acc)
+  foldr
+    ($)
     t
     (replicate (b `mod` 4) rotateTile <> replicate (a `mod` 2) flipTile)
 reverseOp (Op a b) t =
-  foldl'
-    (\acc x -> x acc)
+  foldr
+    ($)
     t
-    (replicate (a `mod` 2) flipTile <> replicate (negate b `mod` 4) rotateTile )
+    (replicate (a `mod` 2) flipTile <> replicate (negate b `mod` 4) rotateTile)
 
 sides :: Tile -> [(Direction, [Bool])]
 sides t =
   map
     (fmap (map (t !)))
     [ (North, [(x, minY) | x <- [minX .. maxX]]),
-      (West, [(maxX, y) | y <- [minY .. maxY]]),
+      (East, [(maxX, y) | y <- [minY .. maxY]]),
       (South, [(x, maxY) | x <- reverse [minX .. maxX]]),
-      (East, [(minX, y) | y <- reverse [minY .. maxY]])
+      (West, [(minX, y) | y <- reverse [minY .. maxY]])
     ]
   where
     ((minX, minY), (maxX, maxY)) = bounds t
@@ -104,7 +130,7 @@ flipTile t =
   let i = bounds t
    in ixmap
         i
-        ( \(x, y) -> (negate x + fst (snd i) - fst (fst i), y)
+        ( \(x, y) -> (negate (x - fst (snd i) + fst (fst i)), y)
         )
         t
 
@@ -169,5 +195,5 @@ day20 = do
   --   . product
   --   . map fst
   --   $ filter ((== Corner) . (`isTypeIn` input') . snd) input
-  putStrLn $ printTile x
-  mapM_ (putStrLn . (\(_, y, z) -> printTile y ++ show z ++ "\n")) $ mapMaybe (sharedSides x) input'
+  print $ (,) <$> minimum <*> maximum $ Map.keys $ picMap input'
+  -- print $ map (`isTypeIn` input') input'
