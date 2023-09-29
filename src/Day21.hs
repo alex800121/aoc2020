fe729958d9dc43f6760a71d3e0cf578df01845ba
alex800121 <alex800121@hotmail.com>
2023-09-29 08:30:41 +0800

    day22

--- src/Day21.hs
+++ src/Day21.hs
@@ -6,10 +6,13 @@ import qualified Data.Map as Map
 import Data.Set (Set)
 import qualified Data.Set as Set
 import Debug.Trace (traceShow)
+import Data.List (intercalate)
 
 type Allergen = String
 
 type Ingredient = String
+
+type AllergenMap = Map Allergen (Set Ingredient)
 
 data Food = Food
   { _ingredient :: Set Ingredient,
@@ -31,8 +34,32 @@ readFood s = Food i a
     i = Set.fromList $ words x
     a = Set.fromList $ splitOn ", " y
 
+allergenMap :: [Food] -> AllergenMap
+allergenMap = Map.unionsWith Set.intersection . map (\(Food x y) -> Map.fromSet (const x) y)
+
+reduceAllergen :: AllergenMap -> AllergenMap
+reduceAllergen m
+  | Map.null single = multiple
+  | otherwise = Map.union single $ reduceAllergen multiple'
+  where
+    (single, multiple) = Map.partition ((== 1) . Set.size) m
+    multiple' = Map.map (Set.\\ (Set.unions $ Map.elems single)) multiple
+
 day21 :: IO ()
 day21 = do
-  input <- Set.fromList . map readFood . lines <$> readFile "input/input21.txt"
-  let allergens = Set.size $ _allergen $ Set.foldl' (\(Food a b) (Food c d) -> Food (Set.union a b) (Set.union c d)) (Food Set.empty Set.empty) input
-  print allergens
+  input <- map readFood . lines <$> readFile "input/input21.txt"
+  let allergens = foldr (\x acc -> Set.union acc (_allergen x))  Set.empty input
+      x = reduceAllergen $ allergenMap input
+      withAllergen = Set.unions $ Map.elems x
+  putStrLn
+    . ("day21a: " ++)
+    . show
+    . sum
+    . map (Set.size . (Set.\\ withAllergen) . _ingredient)
+    $ input
+  putStrLn
+    . ("day21b: " ++)
+    . intercalate ","
+    . map (Set.findMin . snd)
+    . Map.assocs
+    $ x
